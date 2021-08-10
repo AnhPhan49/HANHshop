355d4235be25f75d5aa36283b349ec3c3ce0e05b
AnhPhan49 <phanducanhkg@gmail.com>
2021-08-10 16:15:19 +0700

    fix deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const categoryRoute = require('./categoryRoute')
 const productRoute = require('./productRoute')
 
 const route = app =>{
+    app.get('/',(req,res)=>res.send('Hello'))
     app.use('/api/auth', authRoute)
     app.use('/api/user', accountRoute)
     app.use('/api/category', categoryRoute)
