89e42233490939304c594c212f21e54e2dc11cd9
AnhPhan49 <phanducanhkg@gmail.com>
2021-08-21 21:52:45 +0700

    test deploy first time

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ mongoConnect()
 
 route(app)
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
+    })
+}
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log( `http://localhost:${port}` )
