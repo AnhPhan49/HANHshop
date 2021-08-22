f669623fd76dc5a9b4e5035a6b33edfc9825bfe0
AnhPhan49 <phanducanhkg@gmail.com>
2021-08-22 12:27:00 +0700

    update paht

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const path = require('path')
 app.use(cors())
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
