1b9797185c5ede41d43e1be95cd364b34705819d
Gia Huy <tranvangiahuy1999@gmail.com>
2021-08-24 11:48:39 +0700

    update

--- client/src/views/login.js
+++ client/src/views/login.js
@@ -1,39 +1,54 @@
-import React from 'react'
-import { FormControl, InputLabel, TextField, FormGroup, Button, makeStyles } from '@material-ui/core';
+import React, {useState} from 'react'
+import { FormControl, TextField, FormGroup, Button, makeStyles } from '@material-ui/core';
 import AuthApis from '../apis/authApis'
+import AdminApi from '../apis/adminApis'
 import { useForm } from "react-hook-form";
 import { Link, useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import {savecurrentuserdata} from '../reducers/userReducer'
 import {userloginsuccess} from '../reducers/userReducer'
-
-const useStyles = makeStyles((theme) => ({
-    labelRoot: {
-        fontSize: '1.5rem',
-    },
-    resize: {
-        fontSize: '1.5rem'
-    }
-}))
 
 const Login = (props) => {
     const { register, handleSubmit } = useForm();
     const classes = useStyles()
     const history = useHistory()
     const dispatch = useDispatch();
+    const [btn, setBtn] = useState(false)
+
     const onSubmit = async (data) => {
         try{
+            setBtn(true)
             const res = await AuthApis.login(data);                  
             if(res.status === 200) {             
                 localStorage.setItem("token", res.data)                           
                 AuthApis.setHeaderAxios(res.data)
-                dispatch(userloginsuccess())
-                history.push('/admin')
+                getCurrentUserAndRoute()
             }
         }
         catch(e) {
             console.log(e)
         }
+        setBtn(false)
     }
+
+    const getCurrentUserAndRoute = async () => {
+        try {
+          const res = await AdminApi.getCurrentUser();      
+          if (res.status === 200) {
+            dispatch(savecurrentuserdata(res.data))
+            if(res.data.role === 'admin' || res.data.role === 'manager') {        
+                dispatch(userloginsuccess())
+                history.push('/admin')
+            }
+            else {
+                dispatch(userloginsuccess())
+                history.push('/')
+            }
+          }
+        } catch(e) {
+          console.log(e)
+        }
+      }
 
     return(
         <div >        
@@ -79,7 +94,7 @@ const Login = (props) => {
                             </FormControl>
                         </FormGroup>                            
                         <FormGroup className='mt-5'>
-                            <Button variant="contained" color="primary" type='submit'>
+                            <Button disabled={btn} variant="contained" color="primary" type='submit'>
                                 Login
                             </Button>
                         </FormGroup>
@@ -89,5 +104,14 @@ const Login = (props) => {
         </div>
     )
 }
+
+const useStyles = makeStyles((theme) => ({
+    labelRoot: {
+        fontSize: '1.5rem',
+    },
+    resize: {
+        fontSize: '1.5rem'
+    }
+}))
 
 export default Login
