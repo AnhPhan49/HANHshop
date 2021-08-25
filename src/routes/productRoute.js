9473db10042ad91965846b8ff80a4aad217144ef
AnhPhan49 <phanducanhkg@gmail.com>
2021-08-25 12:14:01 +0700

    update cate

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,27 +1,30 @@
-const express = require('express')
-const productRoute = express.Router()
-const checkLogin = require('../midlewares/loginMidleware')
-const addProductValidator = require('./validators/addProductValidator')
-const checkAdmin = require('../midlewares/AdminMidleware')
-var multipart = require('connect-multiparty')
+const express = require("express");
+const productRoute = express.Router();
+const checkLogin = require("../midlewares/loginMidleware");
+const addProductValidator = require("./validators/addProductValidator");
+const checkAdmin = require("../midlewares/AdminMidleware");
+var multipart = require("connect-multiparty");
 var multipartMiddleware = multipart();
 
 const {
-    createProduct,
-    updateProduct,
-    search,
-    deleteProduct,
-    getProduct
-} = require('../controllers/productController')
+  createProduct,
+  updateProduct,
+  search,
+  deleteProduct,
+  getProduct,
+} = require("../controllers/productController");
 
-productRoute.route('/create').post(checkLogin, multipartMiddleware, addProductValidator, createProduct)
+productRoute.route("/create")
+  .post(checkLogin, multipartMiddleware, addProductValidator, createProduct);
 
-productRoute.route('/update/:id').put(checkLogin, multipartMiddleware, addProductValidator, updateProduct)
+productRoute
+  .route("/update/:id")
+  .put(checkLogin, multipartMiddleware, addProductValidator, updateProduct);
 
-productRoute.route('/search').get(search)
+productRoute.route("/search").get(search);
 
-productRoute.route('/delete/:id').delete(checkLogin, checkAdmin, deleteProduct)
+productRoute.route("/delete/:id").delete(checkLogin, checkAdmin, deleteProduct);
 
-productRoute.route('/get/:id').get( getProduct)
+productRoute.route("/get/:id").get(checkLogin, getProduct);
 
-module.exports = productRoute
+module.exports = productRoute;
--- src/routes/receiptRoute.js
+++ src/routes/receiptRoute.js
@@ -1,14 +1,12 @@
-const express = require('express')
-const receiptRoute = express.Router()
-const checkLogin = require('../midlewares/loginMidleware')
-const checkAdmin = require('../midlewares/AdminMidleware')
+const express = require("express");
+const receiptRoute = express.Router();
+const checkLogin = require("../midlewares/loginMidleware");
+const checkAdmin = require("../midlewares/AdminMidleware");
 
-const {
-    createReceipt
-} = require('../controllers/receiptController')
-
-receiptRoute.route('/create/:id').post(checkLogin, createReceipt)
+const { createReceipt } = require("../controllers/receiptController");
+    
+receiptRoute.route("/create/:id").post(checkLogin, createReceipt);
 
 // receiptRoute.route('/list/:type').get(checkLogin, checkAdmin, listAccount)
 
-module.exports = receiptRoute
+module.exports = receiptRoute;
