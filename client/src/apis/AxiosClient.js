f193d43c703e06346ced052aafe19f2e1a7eca38
Gia Huy <tranvangiahuy1999@gmail.com>
2021-09-05 16:53:47 +0700

    update add manager button

--- client/src/apis/AxiosClient.js
+++ client/src/apis/AxiosClient.js
@@ -1,64 +1,76 @@
-import axios from 'axios';
-import alert from '../utils/alert'
+import axios from "axios";
+import alert from "../utils/alert";
 
 const axiosClient = axios.create({
+  headers: {
     headers: {
-        headers: {
-            'content-type': 'application/json',
-            "Content-Type": "multipart/form-data"},
-    }
-})
+      "content-type": "application/json",
+      "Content-Type": "multipart/form-data",
+    },
+  },
+});
 
 //handle access token
-axiosClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token')
-    config.baseURL = 'https://hanh-shop.herokuapp.com'
-    if(token)        
-        config.headers.Authorization = `Bearer ${token}`
-    return config
-},(error) => Promise.reject(error));
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    config.baseURL = process.env.REACT_APP_BASE_URL;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 //handle error
-const handleError = (error) => {    
-    if(error)
-        alert({icon : 'error',title : 'Error',msg : error.response.data.message})
-    else
-        alert({icon : 'error',title : 'Error',msg : 'No response!'})
-}
+const handleError = (error) => {
+  if (error)
+    alert({ icon: "error", title: "Error", msg: error.response.data.message });
+  else alert({ icon: "error", title: "Error", msg: "No response!" });
+};
 
 //handle response
-axiosClient.interceptors.response.use((res) => {
-    if(res && res.data){
-        return {...res.data, status: res.status}
+axiosClient.interceptors.response.use(
+  (res) => {
+    if (res && res.data) {
+      return { ...res.data, status: res.status };
     }
-    return res
-}, (err) => {
+    return res;
+  },
+  (err) => {
     const statusCode = err.response.status;
-    switch(statusCode) {
-        case 401:
-            window.location.replace('/login')
-            localStorage.removeItem("token")
-            break
-        case 400:            
-            handleError(err)
-            break
-        case 403:
-            alert({icon : 'error',title : 'Error',message: '403 Forbidden' })
-            break
-        case 404:
-            alert({icon : 'error',title : 'Error',message: '404 Not Found' })
-            break
-        case 405:
-            alert({icon : 'error',title : 'Error',message: 'Bạn không thể thao tác vào mục này' })
-            break
-        case 500:
-            alert({icon : 'error',title : 'Error',message: '404 Not Found' })
-             break
-        default:
-            alert({icon : 'error',title : 'Error',msg : '500 Internal Server Error'})
-    
+    switch (statusCode) {
+      case 401:
+        window.location.replace("/login");
+        localStorage.removeItem("token");
+        break;
+      case 400:
+        handleError(err);
+        break;
+      case 403:
+        alert({ icon: "error", title: "Error", message: "403 Forbidden" });
+        break;
+      case 404:
+        alert({ icon: "error", title: "Error", message: "404 Not Found" });
+        break;
+      case 405:
+        alert({
+          icon: "error",
+          title: "Error",
+          message: "Bạn không thể thao tác vào mục này",
+        });
+        break;
+      case 500:
+        alert({ icon: "error", title: "Error", message: "404 Not Found" });
+        break;
+      default:
+        alert({
+          icon: "error",
+          title: "Error",
+          msg: "500 Internal Server Error",
+        });
     }
     return Promise.reject(err);
-})
+  }
+);
 
-export default axiosClient
+export default axiosClient;
--- client/src/views/account-manage-page.js
+++ client/src/views/account-manage-page.js
@@ -1,105 +1,110 @@
-import React, {useState, useEffect} from 'react'
-import {AppBar, useTheme, Tabs, Tab, IconButton } from '@material-ui/core';
-import SwipeableViews from 'react-swipeable-views';
+import React, { useState, useEffect } from "react";
+import { AppBar, useTheme, Tabs, Tab, IconButton } from "@material-ui/core";
+import SwipeableViews from "react-swipeable-views";
 import { IoAddCircle } from "react-icons/io5";
-import {setCustomerData, setManagerData} from '../reducers/accountManagementReducer'
-import { useDispatch } from 'react-redux'
-import AdminApi from '../apis/adminApis'
-import UserManagement from '../components/AccountManagement/user-management'
-import ManagerManagement from '../components/AccountManagement/manager-management'
+import {
+  setCustomerData,
+  setManagerData,
+} from "../reducers/accountManagementReducer";
+import { useDispatch } from "react-redux";
+import AdminApi from "../apis/adminApis";
+import UserManagement from "../components/AccountManagement/user-management";
+import ManagerManagement from "../components/AccountManagement/manager-management";
 
 function a11yProps(index) {
-    return {
-      id: `scrollable-auto-tab-${index}`,
-      'aria-controls': `scrollable-auto-tabpanel-${index}`,
-    };
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    "aria-controls": `scrollable-auto-tabpanel-${index}`,
+  };
 }
 
-const AccountManagePage = () => {    
-    const dispatch = useDispatch();
-    const [value, setValue] = useState(0)    
-    const theme = useTheme();
-    const [open, setOpen] = useState(false)
+const AccountManagePage = () => {
+  const dispatch = useDispatch();
+  const [value, setValue] = useState(0);
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  useEffect(() => {
+    getCustomerList();
+    getManagerList();
+  }, []);
 
-    const handleChangeIndex = (index) => {
-        setValue(index);
-    };    
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    useEffect(() => {
-        getCustomerList()
-        getManagerList()
-    }, [])
+  const handleOpenAddModal = () => {
+    setOpen(true);
+  };
 
-    const handleOpenAddModal = () => {
-        setOpen(true)
+  const handleCloseAddModal = () => {
+    setOpen(false);
+  };
+
+  const closeAfterSave = () => {
+    getManagerList();
+    handleCloseAddModal();
+  };
+
+  const getCustomerList = async () => {
+    try {
+      const res = await AdminApi.getCustomerAccountList();
+      if (res.status === 200) {
+        dispatch(setCustomerData(res.data));
+      }
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const handleCloseAddModal = () => {
-        setOpen(false)
+  const getManagerList = async () => {
+    try {
+      const res = await AdminApi.getManagerAccountList();
+      if (res.status === 200) {
+        dispatch(setManagerData(res.data));
+      }
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const closeAfterSave = () => {
-        getManagerList()
-        handleCloseAddModal()
-    }
-  
-    const getCustomerList = async () => {
-        try {
-            const res = await AdminApi.getCustomerAccountList();
-            if(res.status === 200) {
-                dispatch(setCustomerData(res.data))
-            }
-        } catch(e) {
-            console.log(e)
-        }
-    }
+  return (
+    <div>
+      <AppBar position="static" color="white">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor=""
+          textColor="primary"
+          centered
+        >
+          <Tab label={<h4>Khách hàng</h4>} {...a11yProps(0)} />
+          <Tab label={<h4>Quản lý</h4>} {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={value}
+      >
+        <UserManagement
+          value={value}
+          index={0}
+          dir={theme.direction}
+          reloadData={getCustomerList}
+        ></UserManagement>
+        <ManagerManagement
+          value={value}
+          index={1}
+          dir={theme.direction}
+          open={open}
+          handleCloseAddModal={handleCloseAddModal}
+          closeAfterSave={closeAfterSave}
+          reloadData={getManagerList}
+          handleOpenAddModal={handleOpenAddModal}
+        ></ManagerManagement>
+      </SwipeableViews>
+    </div>
+  );
+};
 
-    const getManagerList = async () => {
-        try {
-            const res = await AdminApi.getManagerAccountList();            
-            if(res.status === 200) {            
-                dispatch(setManagerData(res.data))                
-            }
-        } catch(e) {
-            console.log(e)
-        }
-    }
-
-    return(
-        <div>
-            {
-                value===1 && (
-                    <IconButton className='float-button' color="primary" onClick={handleOpenAddModal}>
-                        <IoAddCircle color='#0C9' size='60px'></IoAddCircle>
-                    </IconButton>
-                )
-            }            
-            <AppBar position="static" color="white">      
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    indicatorColor=""
-                    textColor="primary"
-                    centered
-                >
-                    <Tab label={<h4>Khách hàng</h4>} {...a11yProps(0)} />
-                    <Tab label={<h4>Quản lý</h4>} {...a11yProps(1)} />
-                </Tabs>
-                </AppBar>
-                <SwipeableViews
-                    axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                    index={value}
-                    onChangeIndex={handleChangeIndex}
-                >
-                    <UserManagement value={value} index={0} dir={theme.direction} reloadData={getCustomerList}></UserManagement>
-                    <ManagerManagement value={value} index={1} dir={theme.direction} open={open} handleCloseAddModal={handleCloseAddModal} closeAfterSave={closeAfterSave} reloadData={getManagerList}></ManagerManagement>
-                </SwipeableViews>               
-        </div>
-    )
-}
-
-export default AccountManagePage
+export default AccountManagePage;
